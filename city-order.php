4f7d9ec224c9a0a26488d487742a3b2d40e74311
Ростислав <extra0@mail.ru>
2016-07-03 18:15:06 +0300

    установка яндекс цели

--- city-order.php
+++ city-order.php
@@ -17,12 +17,12 @@ $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 // Формирование тела письма
 $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc; padding-bottom:15px;'>Бронь мест ".$citys."</h2>\r\n";
-$msg .= "<p><strong>Города:</strong> ".$citys."</p>\r\n";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px solid #ccc; padding-bottom:15px;'>Бронь мест ".$citys."</h2>\r\n";
+// $msg .= "<p><strong>Города:</strong> ".$citys."</p>\r\n";
+$msg .= "<p><strong>Дата:</strong> ".$date."</p>\r\n";
 $msg .= "<p><strong>Забрать из:</strong> ".$cityArive."</p>\r\n";
 $msg .= "<p><strong>Отправление:</strong> ".$placeFrom."</p>\r\n";
-$msg .= "<p><strong>Прибытие:</strong> ".$placeTo."</p>\r\n";
-$msg .= "<p><strong>Дата:</strong> ".$date."</p>\r\n";
+// $msg .= "<p><strong>Прибытие:</strong> ".$placeTo."</p>\r\n";
 $msg .= "<p><strong>Кол-во мест:</strong> ".$places."</p>\r\n";
 $msg .= "<p><strong>Цена:</strong> ".$price."</p>\r\n";
 $msg .= "<p><strong>Имя:</strong> ".$name."</p>\r\n";
