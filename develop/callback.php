d232a8c1c3a819cf26d3274f43f114258067f1fe
Ростислав <extra0@mail.ru>
2016-09-28 10:24:39 +0300

    правка модального + корректировка фона на страницах

--- develop/callback.php
+++ develop/callback.php
@@ -1,6 +1,6 @@
 <?php
 $sendto   = "extra0@mail.ru";
-$name 	  = $_POST['name'];
+// $name 	  = $_POST['name'];
 $phone    = $_POST['phone'];
 // Формирование заголовка письма
 $subject  = "Заказ звонка";
@@ -10,8 +10,8 @@ $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 // Формирование тела письма
 $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc; padding-bottom:15px;'>Заказ обратного звонка:</h2>\r\n";
-$msg .= "<p><strong>Имя:</strong> ".$name."</p>\r\n";
+$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc; padding-bottom:15px;'>Заказ звонка:</h2>\r\n";
+// $msg .= "<p><strong>Имя:</strong> ".$name."</p>\r\n";
 $msg .= "<p><strong>Телефон:</strong> ".$phone."</p>\r\n";
 $msg .= "</body></html>";
 
