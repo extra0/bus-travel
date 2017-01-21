63bd37f74dfccc1249c11646ba15913c214bd197
Ростислав <extra0@mail.ru>
2017-01-21 12:50:31 +0200

    правки маршрутов на крым

--- develop/city-order.php
+++ develop/city-order.php
@@ -9,6 +9,7 @@ $places 	  = $_POST['places'];
 $name 	      = $_POST['name'];
 $phone        = $_POST['phone'];
 $price        = $_POST['price'];
+$transfer     = $_POST['transfer'];
 // Формирование заголовка письма
 $subject  = "Бронирование на рейс ".$citys."";
 $headers  = "From: " . strip_tags("order@vseputi.com.ua") . "\r\n";
@@ -18,11 +19,12 @@ $headers .= "Content-Type: text/html;charset=utf-8 \r\n";
 // Формирование тела письма
 $msg  = "<html><body style='font-family:Arial,sans-serif;'>";
 $msg .= "<h2 style='font-weight:bold;border-bottom:1px solid #ccc; padding-bottom:15px;'>Бронь мест ".$citys."</h2>\r\n";
-// $msg .= "<p><strong>Города:</strong> ".$citys."</p>\r\n";
 $msg .= "<p><strong>Дата:</strong> ".$date."</p>\r\n";
 $msg .= "<p><strong>Забрать из:</strong> ".$cityArive."</p>\r\n";
 $msg .= "<p><strong>Отправление:</strong> ".$placeFrom."</p>\r\n";
-// $msg .= "<p><strong>Прибытие:</strong> ".$placeTo."</p>\r\n";
+if (isset($_POST['transfer'])) {
+	$msg .= "<p><strong>Трансфер:</strong> Да</p>\r\n";
+}
 $msg .= "<p><strong>Кол-во мест:</strong> ".$places."</p>\r\n";
 $msg .= "<p><strong>Цена:</strong> ".$price."</p>\r\n";
 $msg .= "<p><strong>Имя:</strong> ".$name."</p>\r\n";
